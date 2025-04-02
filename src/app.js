efc64a32cc22fbba8f391ada4ed9ed7b5d5c0902
anonymousHa04 <harsh97814@gmail.com>
2025-04-02 23:01:07 +0530

    setted uop basic transaction

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// express js boiler plate
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
